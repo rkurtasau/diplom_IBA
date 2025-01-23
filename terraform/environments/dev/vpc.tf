b538fe4fb4eb1437dd6aab6ea49c90546ff96819
romanITst <rkurtasov@gmail.com>
2025-01-23 15:45:22 +0000

    internet_gateway resource replaced, depends_on argument added to internet_gateway resource

--- terraform/environments/dev/vpc.tf
+++ terraform/environments/dev/vpc.tf
@@ -5,6 +5,11 @@ resource "aws_vpc" "main_vpc" {
   }
 }
 
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main_vpc.id
+  depends_on = [aws_vpc.main.vpc]
+} 
+
 resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.1.0/26"
@@ -14,6 +19,3 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.main_vpc.id
-}
