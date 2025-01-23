7e97e7cd70b9778496c2c93db4007c04d29cff44
romanITst <rkurtasov@gmail.com>
2025-01-23 15:42:57 +0000

    resource for VPC added, resource resource for public_subnet added

---
+++ terraform/environments/dev/vpc.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "Main AWS Virtual Private Cloud"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/26"
+  depends_on = [aws_vpc.main_vpc]
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main_vpc.id
+}
