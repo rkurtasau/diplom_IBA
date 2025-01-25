5f8f2bfda8ea38d460cc2a181dc990459382af53
romanITst <rkurtasov@gmail.com>
2025-01-25 17:20:35 +0000

    terraform fmt

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
-  default = "eu-north-1"
-  type = string
+  default     = "eu-north-1"
+  type        = string
   description = "Default AWS region"
 }
 
--- terraform/environments/dev/vpc.tf
+++ terraform/environments/dev/vpc.tf
@@ -15,18 +15,22 @@ resource "aws_internet_gateway" "internet_gateway" {
 }
 
 resource "aws_route_table" "route_table" {
-  vpc_id     = aws_vpc.main_vpc
+  vpc_id     = aws_vpc.main_vpc.id
   depends_on = [aws_vpc.main_vpc]
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
   tags = {
     Name = "Route Table"
   }
 }
 
-
+resource "aws_route_table_association" "public_route" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.route_table.id
+  depends_on     = [aws_route_table.route_table]
+}
 
 resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.main_vpc.id
