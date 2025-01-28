6500a16b276f34e3ff6cd65dc4f5068989cb44f7
romanITst <rkurtasov@gmail.com>
2025-01-28 03:52:43 +0000

    count of instances changed

--- terraform/environments/dev/instance.tf
+++ terraform/environments/dev/instance.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "test" {
   ami = "ami-0368b2c10d7184bc7"
-  instance_type = "t3.micro"
-  count = 2
+  instance_type = "t3.micro" 
   subnet_id     = aws_subnet.public_subnet.id
 }
